d3b519c0270896d9d417ac1f826d84382e518858
AnsonAMS <anson_tohata@hotmail.com>
2020-09-24 16:08:37 +0200

    Updating files

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let apiQuotes = [];
 
+// Show New Quote
+function newQuote(){
+    //Pick a random quote from apiQuotes array
+    const quote = ApiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get Quotes From API
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
-} try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    console.log(apiQuotes);
-} catch (error) {
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+      }  catch (error) {
     // Cath Error Here
+    }
 }
-
-
 // On Load
 getQuotes(); 
