f0fca3e813863917606e881977eb27878053b547
AnsonAMS <anson_tohata@hotmail.com>
2020-09-24 17:26:16 +0200

    `Changing index.html script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
 
 // Show New Quote
-function newQuote(){
+function newQuote() {
     //Pick a random quote from apiQuotes array
     const quote = ApiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     console.log(quote);
@@ -14,7 +21,7 @@ async function getQuotes() {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         newQuote();
-      }  catch (error) {
+      } catch (error) {
     // Cath Error Here
     }
 }
