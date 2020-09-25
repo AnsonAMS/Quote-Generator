2feaaeb71511e623b4ec4f21a04ea2e6905df288
AnsonAMS <anson_tohata@hotmail.com>
2020-09-25 12:14:13 +0200

    Adding loader function

--- script.js
+++ script.js
@@ -7,15 +7,40 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading () {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function loading () {
+    quoteContainer.hidden = false;
+    loader.hidden = falsetrue;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     //Pick a random quote from apiQuotes array
     const quote = ApiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+// Check Quote length to determine styling
+    if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+}   else {
+    quoteText.classList.remove('long-quote');
+}
+quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
@@ -27,3 +52,4 @@ async function getQuotes() {
 }
 // On Load
 getQuotes(); 
+
